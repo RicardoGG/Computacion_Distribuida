51b4b5b50bc94a2db2c969476c001c21796d832e
RicardoGG <ricardo95.garcia@gmail.com>
2016-09-10 19:47:49 -0500

    Representaciones Graficas

--- Grafica.java
+++ Grafica.java
@@ -5,7 +5,7 @@ public class Grafica<T>{
 
 	private class Vertice{
 		public T elemento;
-		LinkedList<Grafica.Vertice> adyacentes;
+		public LinkedList<Grafica.Vertice> adyacentes;
 
 		public Vertice(T e){
 			this.elemento = e;
@@ -13,48 +13,85 @@ public class Grafica<T>{
 		} 
 		
 		public int getGrado(){
-				return adyacentes.size();
+			return adyacentes.size();
 		}
+        public T getElemento(){
+            return this.elemento;
+        }
+
+
+
 	}
 
 	private LinkedList<Vertice> vertices;
 	private int aristas;
+
 
 	public Grafica(){
 		vertices = new LinkedList<Vertice>();
 		aristas = 0;
 	}
 
-	public void inserta(T e){
+
+	public void agrega(T e){
 		Vertice  vertice = new Vertice(e);
 		vertices.add(vertice);
 	}
-	public void conecta(Vertice a, Vertice b) {
-        a.adyacentes.add(b);
-        b.adyacentes.add(a);
+	public void conecta(T a, T b) {
+        Vertice va = new Vertice(a);
+        Vertice vb = new Vertice(b);
+        va.adyacentes.add(vb);
+        vb.adyacentes.add(va);
         aristas ++;
     }
 
-    public double barabasi(Vertice nuevo, Vertice i){
+    private double barabasi(T n, T ie){
+        Vertice i = new Vertice(ie);
+        Vertice nuevo = new Vertice(n);
     	int ki = i.getGrado();
-    	int suma = 0;
+    	int suma=1;
     	for(Vertice v: vertices){
     		suma+=v.getGrado();
-    	}
+        }
     	double res = ki / suma;
     	return res; 
     }
-    public void conectaBarabasi(Vertice nuevo){
-    	Random r = new Random();
+    public void conectaBarabasi(T nuevo){
+        Random r = new Random();
     	double c = 0;
     	double b = 0;
     	for(int i = 0; i<vertices.size(); i++){
-    		b = barabasi(nuevo,vertices.get(i));
+    		b = barabasi(nuevo,vertices.get(i).getElemento());
     		c = r.nextDouble();
     		if(c < b){
-    			conecta(nuevo,vertices.get(i));
+    			conecta(nuevo,vertices.get(i).getElemento());
     		}
     	}
     }
+    private String getVertices(){
+        String s = "";       
+        for(int i=0; i<vertices.size(); i++)
+            s += vertices.get(i).getElemento() + " ";
+        return s;                     
+    }
+
+    private String getArista(){
+        LinkedList<String> ady = new LinkedList<String>();
+        String s= "";
+        for(Vertice v: vertices){
+            s = String.valueOf(v.adyacentes);
+            ady.add(s);
+        }
+        return ady.toString();
+    }
+
+
+    @Override 
+    public String toString(){
+        String s = "La grafica contiene los vertices y aristas : \n [" + getVertices() + "]\n " + " " + getArista();
+
+        return s;
+    }
+
 
 }
